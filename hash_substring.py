aded59d892b2cb72e82df080b42560ef6dc55da6
KristersRicardsAunins <125254175+KristersRicardsAunins@users.noreply.github.com>
2023-04-16 22:32:57 +0300

    submit

--- hash_substring.py
+++ hash_substring.py
@@ -1,18 +1,18 @@
 #221RDC033
 def read_input():
     input_type = input().strip().upper()
-    if input_type == 'F':
+    if input_type == 'F:
         input_f = "06"
         if 'a' in input_f:
             return
         input_f = "tests/" + input_f
-        with open(input_f) as file: 
+        with open(input_f) as file:
             fred = file.readline()
             txt = file.readline()
-    elif input_type == 'I':
+    elif input_type == 'I:
         fred = input()
-        txt = input() 
-    else: 
+        txt = input()
+    else:
         return
     return (fred, txt)
 
@@ -21,13 +21,13 @@ def print_occurrences(output):
 
 def get_occurrences(pattern, text):
     hush = []
-    numb = []   
+    numb = []
     def find_letter(input_type):
         for i in range(len(hush)):
             if (input_type == hush[i][0]):
                 return hush[i][1]
         return(-1)
-    q = len(pattern)-1
+    q = len(pattern) - 1
     w = len(text)
     e = 100**(q - 1)
     n = 0
