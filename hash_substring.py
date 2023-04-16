e52c06cee3bd548e634fc9d2738c86b6bd2951f9
KristersRicardsAunins <125254175+KristersRicardsAunins@users.noreply.github.com>
2023-04-16 23:08:46 +0300

    test

--- hash_substring.py
+++ hash_substring.py
@@ -2,28 +2,26 @@
 def read_input():
     input_type = input().strip().upper()
     if input_type == 'F':
-        input_f = "tests/06"
+        input_f = "tests/06" #File Path
         with open(input_f) as file:
-            pattern = file.readline()
-            text = file.readline()
+            pattern = file.readline() #First line
+            text = file.readline() #Second line
     elif input_type == 'I':
         pattern = input()
         text = input()
-    else:
-        return
     return (pattern, text)
 
 def print_occurrences(output):
    print(*output, sep=' ')
 
 def get_occurrences(pattern, text):
-    hush = []
-    numb = []
+    hush = [] #To store hash values
+    numb = [] #To store the rolling hash values
     def find_letter(input_type):
         for i in range(len(hush)):
             if (input_type == hush[i][0]):
                 return hush[i][1]
-        return(-1)
+        return(-1) #If hash value is not in the table then return -1
     q = len(pattern) - 1
     w = len(text)
     e = 100**(q - 1)
